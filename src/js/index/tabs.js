d38800c24ebc79ff3eab40a4cc28511bd101b2bd
Serendipityy96 <justserendipity96@gmail.com>
2018-06-17 21:09:16 +0800

    第一页分模块，把数据库的歌曲信息渲染到最新音乐上

--- src/js/index/tabs.js
+++ src/js/index/tabs.js
@@ -12,6 +12,8 @@
             this.view.init();
             this.model = model;
             this.bindEvents();
+            this.Module1();
+            this.Module2();
         },
         bindEvents(){
             this.view.$el.on('click','.tabs-nav li',(e)=>{
@@ -20,7 +22,24 @@
                 $li.addClass('active').siblings().removeClass('active');
                 window.eventHub.emit('selectTab',tabName)
             })
+        },
+        Module1(){
+            let script1 = document.createElement('script');
+            script1.src='js/index/page-1-1.js';
+            script1.onload = function () {
+                console.log('1')
+            }
+            document.body.appendChild(script1)
+        },
+        Module2(){
+            let script2 = document.createElement('script');
+            script2.src='js/index/page-1-2.js';
+            script2.onload = function () {
+                console.log('2')
+            }
+            document.body.appendChild(script2)
         }
     }
+
     controller.init(view,model)
 }
